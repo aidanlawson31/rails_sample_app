1e7eb1257e58eacc8e8579a017e121d8b1d12888
AidanL <aidanlawson31@gmail.com>
2019-05-01 10:48:13 +1000

    Update routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,11 +2,7 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
-  
-  test "should get root" do
+  test "should get home" do
     get root_path
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App"
